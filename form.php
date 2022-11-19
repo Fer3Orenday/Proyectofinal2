5415763cc5cf0b19b236fd49ded0c13df0f9ac09
NeriU <neriduroneu@gmail.com>
2022-11-18 18:54:26 -0600

    intento 1

--- form.php
+++ form.php
@@ -6,7 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
      <!--------------------- ULISES ---------------------------------------------->
-
+     <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+        <style>
+            body{
+                background-color: pink ;
+                font-family: 'Staatliches', cursive; 
+            }
+        </style>
 
 
     <!-- --------------------------------  -->
